16ca753949c190a1a0a0f8a39d16e127ab8cb34f
rexmalebka <rexmalebka@krutt.org>
2025-07-11 17:05:43 -0600

    created context, added avatar logic for saving on localStorage

---
+++ frontend/src/components/XafariContext.jsx
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+
+const XafariContext = createContext({
+  user: {
+    name: null,
+    lastname: null,
+    email: null,
+    avatar: {
+      bodyOptions: 0,
+      hairOptions: 0,
+      clothingOptions: 0,
+      shoeOptions: 0,
+      eyesOptions: 0,
+      glassesAccessoryOptions: 0,
+      headAccessoryOptions: 0,
+      bodyAccessoryOptions: 0,
+    },
+  },
+  setUser: () => {},
+  token: null,
+  setToken: () => {},
+  xecretos: {},
+});
+
+export default XafariContext;
