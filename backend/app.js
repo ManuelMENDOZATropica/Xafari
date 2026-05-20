f587583aabf6f26077711ae34916f3603af40377
manuTototDev <manu@totot.mx>
2026-05-20 10:09:01 -0600

    feat: add CORS middleware to allow cross-origin requests from frontend

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,17 @@ const app = express();
 
 app.use(express.json());
 
+// Enable CORS for all requests to support direct local cross-origin connections
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(routes);
 
 module.exports = app;
